1990814b53a3cca0b74d60af092b7dc564682e26
Tony Lea <tnylea@gmail.com>
2017-09-24 16:21:42 -0700

    First commit

---
+++ src/AwesomeSauceServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AwesomeSauce;
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\ServiceProvider;
+
+class AwesomeSauceServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Add routers and include models/controllers
+        // Add logic here...
+    }
+}
